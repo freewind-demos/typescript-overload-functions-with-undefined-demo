9798c051f9f284be8473e9f762a41a28161f24ec
freewind <nowindlee@gmail.com>
2018-10-26 21:15:15 +0800

    demo

--- hello.ts
+++ hello.ts
@@ -1,3 +1,7 @@
-import {capitalize} from 'lodash';
-
-console.log("Hello, " + capitalize("typescript") + "!");
+function reverse(string: string): string;
+function reverse<T>(array: T[]): T[];
+function reverse<T>(stringOrArray: string | T[]): string | T[] {
+  return typeof stringOrArray === "string"
+      ? stringOrArray.split("").reverse().join("")
+      : stringOrArray.slice().reverse();
+}
