06645913def91bb18e9d4ac1aed26a9edbcd086f
Freewind <nowindlee@gmail.com>
2023-11-09 14:11:51 +0800

    typescript-overload-functions-with-undefined-demo

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 TypeScript Overload Functions With Undefined Demo
 ==================================
 
+https://stackoverflow.com/questions/77443048/overload-functions-with-paramters-of-t-and-t-undefined-in-typescript
+
 如何处理 `T | undefined`类型的参数，让两种情况分开：
 - `T` => `T`
 - `T | undefined`
--- hello.ts
+++ hello.ts
@@ -1,18 +1,10 @@
-function toUpper(value: string | undefined): string | undefined;
+function toUpper(value: undefined): undefined;
 function toUpper(value: string): string ;
 function toUpper(value: string | undefined): string | undefined {
     return value?.toString();
 }
 
-function getStringOrUndefined(): string | undefined {
-    return undefined;
-}
-
-function getString(): string {
-    return 'abc'
-}
-
-const x: string | undefined = toUpper(getStringOrUndefined())
-const y: string = toUpper(getString());
+const x: string = toUpper('abc');
+const y: undefined = toUpper(undefined);
 
 console.log("### ", {x, y})
